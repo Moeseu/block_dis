629d4ea418e90f28ca430aeb7c97c4098c828933
Moeseu <96000890+Moeseu@users.noreply.github.com>
2025-02-08 09:21:25 +0000

    Sat, Feb 8, 2025, 11:21 AM +02:00

--- app.py
+++ app.py
@@ -220,6 +220,7 @@ def register():
     return render_template("register.html")
 
 @app.route("/game/<int:game_id>")
+@login_required
 def game(game_id):
     try:
         # Отримання інформації про гру
